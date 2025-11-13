69030015daf258ad31734fb9211ef08c386cc6db
minhhuetm1-netizen <minhhuetm1@gmail.com>
2025-11-13 23:54:58 +0700

    Add files via upload

--- cơ bản 1.sql
+++ cơ bản 1.sql
@@ -1,1 +1,6 @@
-SELECT * FROM qtkd2_ss5.orders;
+create database Company_DB;
+create table Employees(
+Employees_id INT Primary Key AUTO_INCREMENT,
+First_Name VARCHAR(50),
+Last_Name VARCHAR(50)
+);
